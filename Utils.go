9401f03a156f2b263303c7df28f4d0a910f9f499
stephanrotolante <stephanrotolante@gmail.com>
2024-02-29 20:11:17 -0500

    use OS specific path delimeter

--- Utils.go
+++ Utils.go
@@ -1,16 +1,18 @@
 package main
 
 import (
+	"os"
+	"path"
 	"path/filepath"
 	"strings"
 )
 
 func GetFileDirPath(filePath string) string {
-	splitPath := strings.Split(filePath, "/")
-	return strings.Join(splitPath[:len(splitPath)-1], "/")
+	splitPath := strings.Split(filePath, string(os.PathSeparator))
+	return strings.Join(splitPath[:len(splitPath)-1], string(os.PathSeparator))
 
 }
 
 func CleanFilePath(filePath, newFilePath string) string {
-	return filepath.Clean(filePath + "/" + newFilePath)
+	return filepath.Clean(path.Join(filePath, newFilePath))
 }
